d3cfbbd8b95fe74d835efe36c7e3c40b33835a83
Mathieu Martin <webmat@gmail.com>
2012-03-16 09:31:57 -0400

    oops, comment out the credentials in the model so it causes the issue by default

--- Backup/models/dummy.rb
+++ Backup/models/dummy.rb
@@ -2,8 +2,8 @@ Backup::Model.new(:dummy, 'Sync this dummy setup') do
   sync_with S3 do |s3|
     # Uncomment this to make it work.
     # See https://github.com/meskyanichi/backup/issues/291
-    s3.access_key_id     = ENV['AWS_KEY']
-    s3.secret_access_key = ENV['AWS_SECRET']
+    # s3.access_key_id     = ENV['AWS_KEY']
+    # s3.secret_access_key = ENV['AWS_SECRET']
     s3.bucket            = "backup-issue-291-#{Time.now.to_i}"
 
     s3.concurrency_type  = :threads
